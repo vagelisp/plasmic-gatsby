e37a7a3612d0cf5fe6caf7c38eec7cdd34827d72
Vagelis Papaioannou <info@eboxnet.com>
2022-01-26 17:34:06 +0200

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  siteMetadata: {
-      title: `Gatsboy Test`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
-};
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    plugins: [
+
+    ]
+}
